bae0f73a41074f919281efce020bc2e9464d1921
nevrage <ylan.allouche@gmail.com>
2018-11-20 13:41:16 +0000

    first real

---
+++ auto.bat
@@ -0,0 +1,83 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install -y r.project mpv qutebrowser skype firefox cmder docker docker-toolbox docker-for-windows putty autohotkey python3 rocketchat javaruntime
+(powerbi spf13-vim pip)
+
+merge the current vimrc + auto install
+ibstall rtools the proper way
+
+work on some functional AHK stuff: chats, neomutt, tasks, file manager, R
+
+
+wget arch linux
+programmaticaly setup virtualbox?
+
+cd %USERPROFILE%
+git clone https://github.com/VundleVim/Vundle.vim.git %USERPROFILE%/.vim/bundle/Vundle.vim
+
+install.packages(c("tidyverse","XLConnect"))
+
+add virtualbox to the path for vboxmanage access
+add how I changed the boder and titile bar
+maybe add a theme
+autohotkey: read file, notifications, user input, run, fench accent,
+https://github.com/miraisolutions/winsendkeys
+
+find a way to pass data between cmd/PS and vbox
+
+gimp
+python and pip
+tidyverse
+remote desktop
+folder settings
+rterm in path
+====
+weatherapp
+gimp?
+Python?
+colour and dark theme
+remote desktop
+my own virm
+the JQL.R stuff: arg, second function etc, fix it
+unpin and pins tuff: everything and cmder and the 2 vims and firefore/qutebrowser, the chats, cmd
+wallpapper and colours
+latex and beamer stuff
+jupyter ?
+anaconda
+nvimR
+
+notes arch:
+probably do: openssh and passwd then >
+git
+wget
+N drive stuff
+putty border name etc
+ahk: wm, term, file explorer and close wdinwos
+replace people
+
+also for install:
+rm localtime first
+EoD for chroot session
+update
+password
+no update?
+useradd and ","
+
+
+crash Rterm.exe with NvimR?
+
+ahk shortcuts to open a specific tty/app over tty
+some cmd stuff, basic wm and some cmder stuff
+
+pip install youtube-dl
+
+
+sudo mount.vboxsf -o uid=1000,gid=1000,convertcp=iso8859-1 sharefolder /mount/point
+
+
+taff rprofile around windows flow for work
+
+
+steps and variable
+content BM (ff then at work?) chrome and then start doing some AHK and windows
+repro script 
